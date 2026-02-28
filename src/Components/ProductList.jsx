347ef58c394a2e6ffbca842d813142eac187ceb6
vithursan6 <vithursanm@gmail.com>
2026-02-28 18:46:11 -0500

    added product list functionality

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice';
+
+
 
 const ProductList = () => {
+
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart.cartItems); 
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+  };
+
 
   return (
     <div className="product-list">
@@ -16,9 +28,13 @@ const ProductList = () => {
             {products.map(product => (
             <li key={product.id} className="product-list-item">
             <span>{product.name} - ${product.price}</span>
-            <button>
-                Add to Cart
-            </button>
+            <button
+                className={`add-to-cart-btn ${cartItems.some(item => item.id === product.id) ? 'disabled' : ''}`}
+                onClick={() => handleAddToCart(product)}
+                disa bled={cartItems.some(item => item.id === product.id)}
+                >
+                {cartItems.some(item => item.id === product.id) ? 'Added' : 'Add to Cart'}
+                </button>
             </li>
 ))}
 
